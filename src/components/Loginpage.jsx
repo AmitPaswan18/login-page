e0f62d61e2ed2cd1c15765d9e33b6efa56687725
Amit Paswan <ak9425391@gmail.com>
2024-01-09 16:31:26 +0530

    ui fixed

--- src/components/Loginpage.jsx
+++ src/components/Loginpage.jsx
@@ -4,14 +4,22 @@ import Link from "@mui/material/Link";
 import Grid from "@mui/material/Grid";
 import Box from "@mui/material/Box";
 import { Avatar } from "@mui/material";
-import logo from "./images/img.webp";
+import { useState } from "react";
+import bgimage from "../assets/images/bgimage.webp";
+
+import Input from "@mui/material/Input";
+import InputLabel from "@mui/material/InputLabel";
+import InputAdornment from "@mui/material/InputAdornment";
+import FormControl from "@mui/material/FormControl";
+
+import AccountCircle from "@mui/icons-material/AccountCircle";
 
 import FacebookIcon from "@mui/icons-material/Facebook";
 import TwitterIcon from "@mui/icons-material/Twitter";
 import GoogleIcon from "@mui/icons-material/Google";
-import ToggleSelectar from "./common/ToggleSelector";
-
-import WavingHandIcon from "@mui/icons-material/WavingHand";
+import IconButton from "@mui/material/IconButton";
+import Visibility from "@mui/icons-material/Visibility";
+import VisibilityOff from "@mui/icons-material/VisibilityOff";
 
 import Typography from "@mui/material/Typography";
 import Container from "@mui/material/Container";
@@ -20,20 +28,17 @@ import { createTheme, ThemeProvider } from "@mui/material/styles";
 import { signinsucces } from "../redux/Slices/authlogin";
 import { useDispatch } from "react-redux";
 
-function Copyright(props) {
-  return (
-    <Typography variant="body2" color="white" align="center" {...props}>
-      {"Copyright © "}
-      <Link color="inherit">Login Page</Link> {new Date().getFullYear()}
-      {"."}
-    </Typography>
-  );
-}
-
 const defaultTheme = createTheme();
 
 export default function LoginForm() {
   const dispatch = useDispatch();
+  const [showPassword, setShowPassword] = useState(false);
+  const handleClickShowPassword = () => setShowPassword((show) => !show);
+
+  const handleMouseDownPassword = (event) => {
+    event.preventDefault();
+  };
+
   const handleSubmit = async (event) => {
     event.preventDefault();
 
@@ -53,81 +58,110 @@ export default function LoginForm() {
   return (
     <div className="w-full h-full flex justify-center">
       <div className="w-[60%]  h-[100vh] hidden md:flex  ">
-        <img src={logo} className="h-[100vh]" />
+        <img src={bgimage} className="h-[100vh]" />
       </div>
 
-      <div className="md:w-[40%] w-full h-[100vh] flex justify-center items-center  bg-[#151D5E]">
+      <div className="md:w-[40%] w-full h-[100vh] flex justify-center items-center bg-gradient-to-r from-[#CFCCCC] to-slate-[#E6D2CD]">
         <ThemeProvider theme={defaultTheme}>
           <Container component="main" maxWidth="xs">
             <CssBaseline />
 
             <Box
+              className="shadow-lg shadow-slate-700 border-cyan-800 border p-3 bg-white rounded-lg h-[90%]"
               sx={{
-                paddingTop: 8,
                 display: "flex",
                 flexDirection: "column",
                 alignItems: "center",
               }}>
               <Typography
                 sx={{
-                  color: "white",
                   display: "flex",
-                  fontSize: "1.25rem",
-                  fontFamily: " Poppins",
+                  fontFamily: "Poppins",
+                  marginTop: "40px",
+                  fontSize: "26px",
+                  lineHeight: "1",
                 }}
                 component="h1"
-                variant="h5">
-                Welcome to Login Page!
-                <WavingHandIcon sx={{ color: "white", margin: "0px 5px" }} />
+                variant="h6">
+                Login
               </Typography>
               <Box
                 component="form"
                 onSubmit={handleSubmit}
                 noValidate
-                sx={{ mt: 1, color: "white", width: "80%" }}>
+                sx={{ mt: 1, width: "80%" }}>
                 <div className="mt-2">
-                  <label className="text-[16px] " htmlFor="UserName">
-                    Email:
-                  </label>
-                  <ToggleSelectar
-                    type="text"
-                    className="rounded-md h-8 w-full border-2 bg-[#151D5E] focus:outline-none  text-white  p-4"
-                    name="username"
-                    id="username"
-                    required
-                    autoComplete="UserName"
-                    autoFocus
-                  />
+                  <FormControl
+                    sx={{ width: "90%", marginTop: "20px" }}
+                    variant="standard">
+                    <Input
+                      className="rounded-md h-8 w-full text-[#CBCBCB]  focus:outline-none "
+                      name="username"
+                      placeholder="Username"
+                      id="input-with-icon-adornment"
+                      startAdornment={
+                        <InputAdornment position="start">
+                          <AccountCircle />
+                        </InputAdornment>
+                      }
+                    />
+                  </FormControl>
                 </div>
 
                 <div className="mt-3">
-                  <label className="text-[16px]" htmlFor="Password">
-                    Password:
-                  </label>
-                  <ToggleSelectar
-                    type="password"
-                    className="rounded-md h-8 w-full focus:outline-none text-white p-4 bg-[#151D5E] border-slate-200 border-2"
-                    name="password"
-                    id="password"
-                    required
-                    autoComplete="current-password"
-                  />
+                  <FormControl sx={{ m: 1, width: "90%" }} variant="standard">
+                    <InputLabel
+                      className="text-[#CBCBCB]"
+                      htmlFor="standard-adornment-password">
+                      Password
+                    </InputLabel>
+                    <Input
+                      id="standard-adornment-password"
+                      type={showPassword ? "text" : "password"}
+                      endAdornment={
+                        <InputAdornment position="end">
+                          <IconButton
+                            aria-label="toggle password visibility"
+                            onClick={handleClickShowPassword}
+                            onMouseDown={handleMouseDownPassword}>
+                            {showPassword ? <VisibilityOff /> : <Visibility />}
+                          </IconButton>
+                        </InputAdornment>
+                      }
+                    />
+                  </FormControl>
                 </div>
 
-                <div className="mt-2">
-                  <Link className="flex text-white focus:outline-none justify-end cursor-pointer">
+                <div className="text-sm">
+                  <Link
+                    sx={{ color: "gray" }}
+                    underline="none"
+                    className="flex focus:outline-none justify-end cursor-pointer">
                     {"Forgot Password?"}
                   </Link>
                 </div>
                 <Button
                   type="submit"
                   fullWidth
-                  className="rounded-2xl"
+                  className="bg-gradient-to-r from-fuchsia-500 to-cyan-500"
                   variant="contained"
-                  sx={{ mt: 3, mb: 2, backgroundColor: "blue" }}>
+                  sx={{ mt: 3, mb: 2 }}>
                   Sign In
                 </Button>
-                <div className="flex justify-center">{"Or sign up Using "}</div>
+
+                <Typography
+                  sx={{
+                    display: "flex",
+                    justifyContent: "center",
+                    fontFamily: "Poppins",
+                    marginTop: "15px",
+                    marginBottom: "10px",
+                    fontSize: "14px",
+                  }}
+                  component="h6"
+                  variant="h1">
+                  Or Sign Up Using
+                </Typography>
 
                 <div className="h-fit flex flex-row  justify-center  ">
                   <Avatar sx={{ m: 1, bgcolor: "blue" }}>
@@ -147,19 +181,15 @@ export default function LoginForm() {
                     </button>
                   </Avatar>
                 </div>
-                <Grid container justifyContent="center">
+                <Grid container justifyContent="center" marginBottom="30px">
                   <Grid item>
-                    <Link
-                      sx={{ color: "white" }}
-                      href="/signup"
-                      variant="body2">
+                    <Link underline="none" href="#" variant="body2">
                       {"Don't have an account? Sign Up"}
                     </Link>
                   </Grid>
                 </Grid>
               </Box>
             </Box>
-            <Copyright sx={{ mt: 8, mb: 4 }} />
           </Container>
         </ThemeProvider>
       </div>
